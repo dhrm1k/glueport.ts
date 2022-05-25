68e06b1ac59c8ca6df48cf3ceafd5a243d44f359
Dharmik Parmar <75156210+itsdharmik@users.noreply.github.com>
2022-05-25 10:18:30 +0000

    Initial commit

---
+++ .build/index.js
@@ -0,0 +1,10 @@
+function disp_details(id, name, mail_id) {
+  console.log("ID:", id);
+  console.log("Name:", name);
+  if (mail_id != void 0)
+    console.log("Email Id", mail_id);
+  console.log("");
+}
+disp_details(123, "John");
+disp_details(111, "mary", "mary@xyz.com");
+//# sourceMappingURL=index.js.map
