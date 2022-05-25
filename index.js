305b8547c7ee0cc7fd324a7efb60925d3c255b51
Dharmik Parmar <75156210+itsdharmik@users.noreply.github.com>
2022-05-25 10:35:39 +0000

    so cool. it works.

--- index.js
+++
@@ -1,9 +0,0 @@
-function disp_details(id, name, mail_id) {
-    console.log("ID:", id);
-    console.log("Name:", name);
-    if (mail_id != undefined)
-        console.log("Email Id", mail_id);
-    console.log("");
-}
-disp_details(123, "John");
-disp_details(111, "mary", "mary@xyz.com");
